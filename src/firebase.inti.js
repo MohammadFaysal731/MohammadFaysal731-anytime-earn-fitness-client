dd8d88dd8c127c80d272c3f386d978394f5cb5c6
MohammadFaysal731 <www.faysal731@gmail.com>
2023-03-26 11:40:16 +0600

    add env file

--- src/firebase.inti.js
+++ src/firebase.inti.js
@@ -4,17 +4,17 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD11LfNcPyKXLX5hvce4bhhQJlCFlIt0bM",
-  authDomain: "anytime-earn-fitness-client.firebaseapp.com",
-  projectId: "anytime-earn-fitness-client",
-  storageBucket: "anytime-earn-fitness-client.appspot.com",
-  messagingSenderId: "895890796443",
-  appId: "1:895890796443:web:a6e0a3c95fc2af58b7fb3e",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_apiId,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 const auth =getAuth(app);
